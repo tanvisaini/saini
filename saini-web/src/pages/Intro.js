ef91413db4be79e0563c81c556719b7e5165c062
Tanvi Saini <ts4175@nyu.edu>
2024-01-16 17:02:49 -0500

    title

--- saini-web/src/pages/Intro.js
+++ saini-web/src/pages/Intro.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Cursor from '../components/Cursor'
+// import Cursor from '../components/Cursor'
 
 const Intro = () => {
     return (
         <div> 
-            <Cursor />
+            {/* <Cursor /> */}
             <h1 class="text-8xl font-eb text-center"> Tanvi Saini </h1>
         </div>
     );
