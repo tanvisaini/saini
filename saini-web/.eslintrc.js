ded4b28174bee22dc91fa1231eee626c1445147e
Tanvi Saini <ts4175@nyu.edu>
2024-07-06 19:19:11 -0400

    alt prop

--- saini-web/.eslintrc.js
+++ saini-web/.eslintrc.js
@@ -1,10 +1,9 @@
-.eslintrc.js
 module.exports = {
     extends: ['eslint:recommended', 'plugin:react/recommended'],
     plugins: ['react', 'jsx-a11y'],
     rules: {
-      // ...other rules
       'jsx-a11y/alt-text': ['error', { elements: ['img', 'object', 'area', '[role=img]'] }],
+      // ...other rules
     },
   };
   
