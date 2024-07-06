e5b6f9eb0fb30d573285139e4fcce2763c0d24b1
Tanvi Saini <ts4175@nyu.edu>
2024-07-06 19:25:24 -0400

    prop type

--- saini-web/.eslintrc.js
+++ saini-web/.eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    plugins: ['react', 'jsx-a11y'],
     rules: {
       'jsx-a11y/alt-text': 'off',
-      // ...other rules
+      'react/prop-types': 'off',
     },
   };
   
